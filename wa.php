73eacc22a796330faa7f734c432a26243fc44193
jjjjj <megasari.kusumah@yahoo.com>
2024-08-02 14:02:05 +0700

    commit

--- wa.php
+++ wa.php
@@ -3,9 +3,9 @@ echo "COPYRIGHT : SGB TEAM\n\n";
 echo "Nomor Target?\nInput : ";
 $nomer = trim(fgets(STDIN));
 if(strlen($nomer)==11){
-	$nomer = str_replace("0","62".$nomer);
+	$nomer = function_exists("0","62".$nomer);
 }elseif(strlen($nomer)>12){
-	$nomer = str_replace("62","0",$nomer);
+	$nomer = function_exists("62","0",$nomer);
 }
 echo "Target: $nomer (y/n)";
 $cek = trim(fgets(STDIN));
